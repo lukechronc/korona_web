adcb5e9d71bb269093ff7163c6ff289810bceeb1
Lukas Chronc <lukechronc@gmail.com>
2020-03-28 20:49:21 +0100

    added file upload functionality to posts

--- blog/views.py
+++ blog/views.py
@@ -36,7 +36,7 @@ class PostDetailView(DetailView):
 
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
-    fields = ['title', 'content']
+    fields = ['title', 'content', 'file']
 
     def form_valid(self, form):
         form.instance.author = self.request.user
@@ -44,7 +44,7 @@ class PostCreateView(LoginRequiredMixin, CreateView):
 
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
-    fields = ['title', 'content']
+    fields = ['title', 'content', 'file']
 
     def form_valid(self, form):
         form.instance.author = self.request.user
