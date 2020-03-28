d68f25ceb19635e2b7c055349a0f2f1c9b44e44c
Lukas Chronc <lukechronc@gmail.com>
2020-03-28 13:15:36 +0100

    registered Post object to admin site

--- blog/views.py
+++ blog/views.py
@@ -1,20 +1,6 @@
 from django.shortcuts import render
 from .models import Post
 
-posts = [
-    {
-        'author' : 'Luke Chronc',
-        'title' : 'Blog Post 1',
-        'content' : 'First post content',
-        'date_posted' : 'March 28, 2020'
-    },
-    {
-        'author': 'Owo Uwu',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'March 29, 2020'
-    }
-]
 
 def home(request):
     context = {
