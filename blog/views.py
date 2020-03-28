da830a1729c5608338eb43cbb33076011c70fffa
Lukas Chronc <lukechronc@gmail.com>
2020-03-28 01:32:19 +0100

    minor changes, added views for blog and about using template inheretance

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author' : 'Luke Chronc',
+        'title' : 'Blog Post 1',
+        'content' : 'First post content',
+        'date_posted' : 'March 28, 2020'
+    },
+    {
+        'author': 'Owo Uwu',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'March 29, 2020'
+    }
+]
 
 def home(request):
-    return HttpResponse('<h>Blog Home</h1>')
-    
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+def about(request):
+    return render(request, 'blog/about.html',{'title':'OwO'})
